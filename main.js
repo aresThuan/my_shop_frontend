e86f544fc14974ddd5e0c07aa5a4c38f09fb5317
aresThuan <ares.thuan@gmail.com>
2024-08-26 19:16:11 +0700

    Starting learn about Javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+// BUILD-IN FUNCTION ================================
+// 1. alert => notification
+// 2. console => show message to console
+// 3. confirm => get confirm from user by dialog
+// 4. prompt => get input fron user by dialog
+// setTimeout => run a function 1 time after a while
+// setInterval =>  same like setTimeout but it run non-stop every 1s
